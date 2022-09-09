393b8c296d0b23071a44b869a38073d034ec4648
Vikky234 <victortony414@gmail.com>
2022-09-09 13:28:10 -0700

    Task 0 commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+	int num;
+
+	printf("Input a number :");
+	if (num >= 0)
+	   printf("%d is a positive number \n", num);
+	else
+	   printf("%d is a negative number \n", num);
+}
+
