4ed58109bbb4119621e01b54d9c2c2104c7433ed
Vikky234 <victortony414@gmail.com>
2022-09-09 18:29:15 -0700

    Task 12 commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print possible combination of two 2-digit
+ *
+ * numbers
+ *
+ * Return: returns zero at the end
+ */
+int main(void)
+{
+int i, p;
+for (i = 0; i < 100; i++)
+{
+for (p = 0; p < 100; j++)
+{
+if (p > i)
+{
+	putchar((i / 10) + '0');
+	putchar((i % 10) + '0');
+	putchar(' ');
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+if (i != 98)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
