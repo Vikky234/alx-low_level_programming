1b52e30fea02e1a2806a441ecd99ca9e8b28cc19
Vikky234 <victortony414@gmail.com>
2022-09-09 17:50:30 -0700

    Task 11 commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,8 @@ for (m = 2; m <= 9; m++)
 if (m > p && p > i)
 {
 	putchar(i + '0');
-	putchar(p + '0');								putchar(m + '0');
+	putchar(p + '0');
+	putchar(m + '0');
 if (i != 7)
 {
 	putchar(',');
