8d22877ded62574889e17655d83e9c113c56896b
Vikky234 <victortony414@gmail.com>
2022-09-14 17:20:00 -0700

    Task 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,20 +9,18 @@ int main(void)
 {
 	int count = 2;
 	long int i = 1, j = 2;
-	long int k;
-	
+	long int k;	
 	printf("%lu, ", i);
 	while (count <= 50)
 	{
 		if (count == 50)
 		{
-			printf("%lu\n",j);
+			printf("%lu\n", j);
 		}
 		else
 		{
 			printf("%lu, ", j);
 		}
-		
 		k = j;
 		j += i;
 		i = k;
