c4600bb4e825b0110516d217be27d3529b4bed31
Vikky234 <victortony414@gmail.com>
2022-09-16 20:01:16 -0700

    Task 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,6 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	
 	i = 0;
 	/*
 	 * while (i < 10)
@@ -16,7 +15,6 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 * /
-	 
 	 printf("Infinite loop avoided! \\o/\n");
 	 return (0);
 }
