47007447d6eefba56c7087857870e62ab29e102c
Vikky234 <victortony414@gmail.com>
2022-09-28 15:04:29 -0700

    Task 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * is_palindrome - is palindrome
+ * @s: string
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	return (pali(s, s + _strlen_recursion(s) - 1));
+}
+/**
+ * pali - is palindrome
+ * @s: start of string
+ * @e: end of string
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int pali(char *s, char *e)
+{
+	if (*s != *e)
+		return (0)
+	else if (s >= e)
+		return (1);
+	else
+		return (pali(s + 1, e - 1));
+}
+/**
+ * _strlen_recursion - return the length of a string
+ * @s: string
+ * Return: length of str
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
