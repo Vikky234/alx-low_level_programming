ccb324e45d9f8b2fd7f6b015b6da7c233748ba6c
Vikky234 <victortony414@gmail.com>
2022-10-13 06:47:55 -0700

    Hacking

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,9 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned char *p = (unsigned char *)main;
-	unsigned int bytes;
-	int i;
+	int bytes, i;
+	char *arr
 
 	if (argc != 2)
 	{
@@ -24,9 +23,22 @@ int main(int argc, char *argv[])
 		exit(2);
 	}
 	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
 	for (i = 0; i < bytes; i++)
-		printf("%02x ", *p++);
-	printf("\n");
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
 	return (0);
 }
 
