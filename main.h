38b2d151364129266a777b6a425634614bacdfbc
Vikky234 <victortony414@gmail.com>
2022-10-27 16:17:27 -0700

    All

--- main.h
+++ main.h
@@ -1,14 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+/*
+ * File: main.h
+ * AUth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ * written in the 0x13-bit_manipulation directory.
+ */
 int _putchar(char c);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int only_nums(char *s);
-int _atoi(char *s);
-void print_string(char s[]);
-void print_int(unsigned int n);
-int _strlen_recursion(char *s);
-#endif
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+#endif /* MAIN_H */
+
